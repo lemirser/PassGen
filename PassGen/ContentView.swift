679fce29599a8bdf9e13305429f7b2302333f10e
lemirser <serzoleandro@gmail.com>
2026-07-25 05:58:06 +0800

    WIP: script implementation in progress

---
+++ PassGen/ContentView.swift
@@ -0,0 +1,88 @@
+//
+//  ContentView.swift
+//  PassGen
+//
+//  Created by Leandro Serzo on 7/23/26.
+//
+
+import SwiftUI
+
+
+// Add functionality to set the menubar as active when the toolbar is open
+struct ContentView: View {
+    @State private var withCapital = false
+    @State private var withNumbers = false
+    @State private var withSymbols = false
+    @State private var passwordLength : Int = 16
+    @State private var lengthText = "16"
+    
+    let lowercase = "abcdefghijklmnopqrstuvwxyz"
+    
+    var capitalBinding: Binding<Bool> {
+        Binding(
+            get:{withCapital},
+            set: {newValue in
+                if newValue == false && withNumbers == false && withSymbols == false {
+                    return
+                }
+                withCapital = newValue
+            }
+        )
+    }
+    
+    var numbersBinding: Binding<Bool> {
+        Binding(
+            get:{withNumbers},
+            set: {newValue in
+                if newValue == false && withCapital == false && withSymbols == false {
+                    return
+                }
+                withNumbers = newValue
+            }
+        )
+    }
+    
+    var symbolsBinding: Binding<Bool> {
+        Binding(
+            get:{withSymbols},
+            set: {newValue in
+                if newValue == false && withCapital == false && withNumbers == false {
+                    return
+                }
+                withSymbols = newValue
+            }
+        )
+    }
+    
+    var body: some View {
+        VStack {
+            Text(lowercase)
+            Text("Strength: strong")
+            
+            TextField("Length", text:$lengthText)
+                .onChange(of: lengthText, {oldValue,
+                    newValue in lengthText = newValue.filter {$0.isNumber}
+                }
+                )
+                .onSubmit {
+                    var value = Int(lengthText) ?? 16
+                    if value < 8 { value = 8 }
+                    if value > 64 { value = 64 }
+                    passwordLength = value
+                    lengthText = String(value)
+                    }
+            
+            Toggle("Capital Letters", isOn: capitalBinding)
+            Toggle("Numbers", isOn: numbersBinding)
+            Toggle("Symbols", isOn: symbolsBinding)
+            
+            Button("Copy"){}
+            Button("Regenerate"){}
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PassGen/PassGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassGenApp.swift
+//  PassGen
+//
+//  Created by Leandro Serzo on 7/23/26.
+//
+
+import SwiftUI
+
+@main
+struct PassGenApp: App {
+    var body: some Scene {
+        MenuBarExtra("PassGen") {
+            ContentView()
+        }.menuBarExtraStyle(.window)
+    }
+}
